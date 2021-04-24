08c5b3d908fd360379b6b1c858de497ff33d1848
profrog <profrog@kakao.com>
2021-04-24 16:27:24 +0900

    week-6 assign

--- code/week-6/GNB/classifier.py
+++ code/week-6/GNB/classifier.py
@@ -33,6 +33,42 @@ class GNB():
         Collect the data and calculate mean and standard variation
         for each class. Record them for later use in prediction.
         '''
+
+        label_temp = {}
+        label_temp['left'] = []
+        label_temp['keep'] = []
+        label_temp['right'] = []
+
+        for label in self.classes:
+            for i in range(4):
+                label_temp[label].append([])
+        
+        for i in range(len(X)):
+            x = X[i]
+            label = Y[i]
+
+            x = self.process_vars(x)
+
+            for index, val in enumerate(x):
+                label_temp[label][index].append(val)
+            
+        mean = []
+        std = []
+
+        for i in self.classes:
+            mean.append([])
+            std.append([])
+
+            for v in label_temp[i]:
+                m = np.mean(v)
+                s = np.std(v)
+
+                mean[-1].append(m)
+                std[-1].append(s)
+        
+        self.means = mean
+        self.stds = std
+
         # TODO: implement code.
 
     # Given an observation (s, s_dot, d, d_dot), predict which behaviour
@@ -46,5 +82,36 @@ class GNB():
         Return the label for the highest conditional probability.
         '''
         # TODO: implement code.
-        return "keep"
+
+        prob = []
+        observation = self.process_vars(observation)
+        for i in range(len(self.means)):
+            m = self.means[i]
+            s = self.stds[i]
+            l = self.classes[i]
+
+            product = 1
+
+            for j in range(len(m)):
+                mu = m[j]
+                sig = s[j]
+                o = observation[i]
+
+                liklihood_temp = gaussian_prob(o,mu,sig)
+                product = product * liklihood_temp
+            prob.append(product)
+        sum_prob = sum(prob)
+
+        probs = [ p/sum_prob for p in prob]
+
+        index = 0
+        best = 0
+
+        for i, p in enumerate(probs):
+            if p > best:
+                best = p
+                index = 1
+        names = ['left','keep','right']
+        # return "keep"
+        return names[index]
 
